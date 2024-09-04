9ef22227d47add44cd109d08e12ade94fe749203
nc-john-hutcheon <john.hutcheon@northcoders.com>
2024-09-04 11:39:22 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import "./App.css";
+
+function App() {
+  const tvList = ["Dark", "Succession", "Inside No.9"];
+
+  return (
+    <section className="App">
+      <header>
+        <h1>AmaJohn Prime</h1>
+        <h2>My watchlist</h2>
+      </header>
+      <section>
+        <ul>
+          {tvList.map((tvShow) => {
+            return <li key={tvShow}>{tvShow}</li>;
+          })}
+        </ul>
+      </section>
+      <footer>
+        <form>
+          <input placeholder="Next show to watch..." type="text" id="tvshow" />
+          <input type="submit" value="Add to list" />
+        </form>
+      </footer>
+    </section>
+  );
+}
+
+export default App;
---
+++ src/Components/TVList.jsx
@@ -0,0 +1,3 @@
+const TVList = () => {};
+
+export default TVList;
